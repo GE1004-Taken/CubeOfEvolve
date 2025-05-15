929b0d857c9c8cf759598654409717a305c9f2fb
Ahyahyahya <u9b3d5kuesehbns6s6h7@docomo.ne.jp>
2025-05-15 17:56:49 +0900

    ゲームステートの実装

---
+++ Assets/01_Game/Scripts/GameManagers/GameStateEnum.cs
@@ -0,0 +1,13 @@
+namespace Assets.IGC2025.Scripts.GameManagers
+{
+    public enum GameState
+    {
+        TITLE,
+        INITIALIZE,
+        GAME,
+        PAUSE,
+        TUTORIAL,
+        GAMEOVER,
+        GAMECLEAR
+    }
+}
