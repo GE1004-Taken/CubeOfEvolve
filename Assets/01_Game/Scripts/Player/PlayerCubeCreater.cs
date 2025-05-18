74d3c6c99810cd4f6e2aa86cc8ae8874b3180340
Ahyahyahya <u9b3d5kuesehbns6s6h7@docomo.ne.jp>
2025-05-19 03:06:01 +0900

    設置可能表示実装

--- Assets/01_Game/Scripts/Player/PlayerCubeCreater.cs
+++ Assets/01_Game/Scripts/Player/PlayerCubeCreater.cs
@@ -9,9 +9,17 @@ public class PlayerCubeCreater : BasePlayerComponent
     [SerializeField] private Cube _cubePrefab;
     [SerializeField] private float _rayDist = 50f;
 
+    [SerializeField] private Material _normalMaterial;
+    [SerializeField] private Material _trueMaterial;
+    [SerializeField] private Material _falseMaterial;
+
+    // ---------- RP
+    private bool _canCreated = new();
+
     // ---------- Field
+    public Cube _predictCube = null;
+    private MeshRenderer _predictCubeMeshRenderer;
     private Vector3 _createPos;
-    private bool _canCreated;
 
     // ---------- UnityMessage
     protected override void OnInitialize()
@@ -28,13 +36,40 @@ public class PlayerCubeCreater : BasePlayerComponent
                 {
                     if (hit.collider.TryGetComponent<Cube>(out var cube))
                     {
-                        _canCreated = true;
-
                         _createPos = cube.transform.position + hit.normal;
+
+                        // 設置予測キューブを生成
+                        if (_predictCube == null)
+                        {
+                            _predictCube = Instantiate(
+                                    _cubePrefab,
+                                    _createPos,
+                                    transform.rotation);
+
+                            _predictCube.transform.SetParent(transform);
+
+                            _predictCubeMeshRenderer = _predictCube.GetComponent<MeshRenderer>();
+
+                            _predictCubeMeshRenderer.material = _trueMaterial;
+                        }
+                        // 設置予測キューブの位置を更新
+                        else
+                        {
+                            _predictCube.transform.position = _createPos;
+                        }
+
+                        _canCreated = true;
                     }
                     else
                     {
                         _canCreated = false;
+
+                        if (_predictCube != null)
+                        {
+                            _predictCubeMeshRenderer.material = _falseMaterial;
+
+                            Destroy(_predictCube.gameObject);
+                        }
                     }
                 }
             });
@@ -44,12 +79,9 @@ public class PlayerCubeCreater : BasePlayerComponent
             .Where(x => _canCreated)
             .Subscribe(_ =>
             {
-                var createCube = Instantiate(
-                            _cubePrefab,
-                            _createPos,
-                            transform.rotation);
-
-                createCube.transform.SetParent(transform);
+                _predictCube.ActiveCube();
+                _predictCubeMeshRenderer.material = _normalMaterial;
+                _predictCube = null;
             });
     }
 }
