52fbfb8490907777e812e06e9ee1e2039c8bb175
DESKTOP-SQJO6MQ\user <raguna1115@gmail.com>
2025-05-20 15:17:50 +0900

    エネミーのAI仮実装

--- Assets/01_Game/Scripts/Enemy/Move/EnemyMove_Ground.cs
+++ Assets/01_Game/Scripts/Enemy/Move/EnemyMove_Ground.cs
@@ -1,7 +1,14 @@
+using UnityEngine;
+
 public class EnemyMove_Ground : EnemyMoveBase
 {
+    [SerializeField] private float _minDistance;
+
     public override void Move()
     {
-        LinearMovement();
+        if (Vector3.Distance(_targetObj.transform.position, transform.position) >= _minDistance)
+        {
+            LinearMovement();
+        }
     }
 }
