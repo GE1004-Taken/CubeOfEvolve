96b58345bf412d23575ef056ddf4d374db883fd3
TAKETO_ANNAKA <antake1226@outlook.jp>
2025-06-03 00:47:23 +0900

    追加 / ステータス設定クラス

---
+++ Assets/01_Game/Scripts/DataBase/ModuleParameter.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class ModuleParameter
+{
+    // -----SerializeField
+    [SerializeField] private float _attack; 
+    [SerializeField] private float _bulletSpeed; 
+    [SerializeField] private float _interval; 
+    [SerializeField] private float _searchRange;
+
+    // -----Property
+    public float Attack => _attack;
+    public float BulletSpeed => _bulletSpeed;
+    public float Interval => _interval;
+    public float SearchRange => _searchRange;
+
+}
