4e61460e86c5c3c3527820421daad586a0efa15c
TAKETO_ANNAKA <antake1226@outlook.jp>
2025-06-18 11:53:18 +0900

    編集 / 記述整理

--- Assets/01_Game/Scripts/Sound/SoundSample.cs
+++ Assets/01_Game/Scripts/Sound/SoundSample.cs
@@ -1,26 +1,55 @@
 using UnityEngine;
 using System.Collections;
 
-namespace SoundManagerSample.TestPlay
+namespace Assets.AT
 {
     public class SoundSample : MonoBehaviour
     {
+        [SerializeField] private GameObject _soundSourceObj;
+
+        private SoundManager SM = SoundManager.Instance;
         private bool _isPlay = true;
 
-        // Update is called once per frame
-        void Update()
+
+        private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space) && _isPlay)
+            if (Input.GetKeyDown(KeyCode.R) && _isPlay) // 音鳴らす
             {
-                SoundManager.Instance.Play("SampleSE", "SE");
+                SM.Play("SampleSE", "SE");
+                _isPlay = false;
+                StartCoroutine(ResetLogFlag());
+            }
+
+            if (Input.GetKeyDown(KeyCode.F) && _isPlay) // ブッピガン！ 指定箇所から
+            {
+                SM.PlaySFXAt("SampleSE", _soundSourceObj, "SE");
+                _isPlay = false;
+                StartCoroutine(ResetLogFlag());
+            }
+
+            if (Input.GetKeyDown(KeyCode.T) && _isPlay) // bgm消すフェード
+            {
+                SM.StopBGMWithFade(1f);
+                _isPlay = false;
+                StartCoroutine(ResetLogFlag());
+            }
+
+            if (Input.GetKeyDown(KeyCode.G) && _isPlay) // bgm鳴らす
+            {
+                SM.PlayBGM("SampleBGM", "BGM", 3f);
                 _isPlay = false;
                 StartCoroutine(ResetLogFlag());
             }
         }
 
+        private void OnDestroy()
+        {
+            StopCoroutine(ResetLogFlag());
+        }
+
         private IEnumerator ResetLogFlag()
         {
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(0.5f);
             _isPlay = true;
         }
     }
