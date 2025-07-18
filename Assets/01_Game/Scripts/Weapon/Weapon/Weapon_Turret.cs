208aae139fa6645452319915abb27801bcab44a7
Koki Sekituka <kss-23130131@nsgcl.jp>
2025-07-18 11:18:12 +0900

    武器の回転部分を指定できるようにした

--- Assets/01_Game/Scripts/Weapon/Weapon/Weapon_Turret.cs
+++ Assets/01_Game/Scripts/Weapon/Weapon/Weapon_Turret.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 public class Weapon_Turret : WeaponBase
 {
     // ---------------------------- SerializeField
-    [Header("モデル")]
-    [SerializeField] private GameObject _model;
-
     [Header("弾")]
     [SerializeField] private Transform _bulletSpawnPos;
     [SerializeField] private Bullet_Linear _bulletPrefab;
@@ -15,15 +12,6 @@ public class Weapon_Turret : WeaponBase
     protected override void Attack()
     {
         var target = _layerSearch.NearestTargetObj.transform;
-
-        // 砲台の回転はY軸のみ（高さを無視して水平方向に向ける）
-        Vector3 flatTargetPos = new Vector3(target.position.x, transform.position.y, target.position.z);
-        Vector3 turretDir = (flatTargetPos - transform.position).normalized;
-
-        if (turretDir != Vector3.zero)
-        {
-            _model.transform.rotation = Quaternion.LookRotation(turretDir);
-        }
 
         // 弾の方向は正確な3D方向（高さも含む）
         Vector3 shootDir = (target.position - _bulletSpawnPos.position).normalized;
